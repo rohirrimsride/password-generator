f1a92e70694064d8dbc8ca691495ed9c5f8fbfe0
Strider <dave.barnes23@gmail.com>
2022-05-19 15:30:14 -0500

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
 // Assignment code here
+// determine a password length (8-128)
+// determine password contents, letters Upper and lower case, numbers, spec. chars. 
+// 
+function generatePassword() {
+  // determine password length
+  var passwordLength = window.prompt("What length would you like your password? Choose a number between 8 and 128.");
+  console.log(passwordLength);
+  passwordLength = parseInt(passwordLength);
+  while (passwordLength < 8 && passwordLength > 128) {
+    window.alert("Please select a number from 8 to 128");
+    return (passwordLength);
+  }
+  for (var i = 0)
+  
+passwordCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$%&*-+"];
+var passwordCharacters;
+
+}
 
 
 // Get references to the #generate element
